d7074aac0e64340a0333c669e1c755a58a3fb95c
Umar Asif <umarasif737@gmail.com>
2023-01-29 16:21:59 +0500

    working on hamburger

--- components/header.js
+++ components/header.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { useRef, useState } from "react";
-//   const div = divRef.current;
-//   const position = div.getBoundingClientRect();
+import { FaBars } from "react-icons/fa";
+
 const Header = () => {
   const [hoveredItem, setHoveredItem] = useState("");
   const [cursorPosition, setCursorPosition] = useState({ x: 0 });
+  const [isOpen, setIsOpen] = useState(false);
   const divRef = useRef(null);
 
   const handleMouseEnter = (e, title) => {
@@ -21,98 +22,112 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <div className="container">
-        <div className="container-items">
-          <div className="logo-items-container">
-            <div className="logo">
-              <Link href="/">
-                <img src="./logo.png" alt="logo" />
-              </Link>
+    <>
+      <header>
+        <div className="container">
+          <div className="container-items">
+            <div className="logo-items-container">
+              <div className="logo">
+                <Link href="/">
+                  <img src="./logo.png" alt="logo" />
+                </Link>
+              </div>
+              <nav className="top-navigation">
+                <ul className="nav-items">
+                  <li
+                    ref={divRef}
+                    onMouseEnter={(e) => handleMouseEnter(e, "home")}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    Agent's Home
+                  </li>
+                  <li
+                    ref={divRef}
+                    onMouseEnter={(e) => handleMouseEnter(e, "account")}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    My Account
+                  </li>
+                  <li
+                    ref={divRef}
+                    onMouseEnter={(e) => handleMouseEnter(e, "about")}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    About
+                  </li>
+                  <li
+                    ref={divRef}
+                    onMouseEnter={(e) => handleMouseEnter(e, "insurance")}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    Insurance
+                  </li>
+                  <li
+                    ref={divRef}
+                    onMouseEnter={(e) => handleMouseEnter(e, "news")}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    News
+                  </li>
+                </ul>
+              </nav>
             </div>
-            <nav className="top-navigation">
-              <ul className="nav-items">
+            <div className="contact-container">
+              <ul className="top">
                 <li
+                  className="phone-number"
                   ref={divRef}
-                  onMouseEnter={(e) => handleMouseEnter(e, "home")}
+                  onMouseEnter={(e) => handleMouseEnter(e, "phone")}
                   onMouseLeave={handleMouseLeave}
                 >
-                  Agent's Home
+                  <a className="a-standalone">(937) 374-0855</a>
                 </li>
                 <li
+                  className="contact"
                   ref={divRef}
-                  onMouseEnter={(e) => handleMouseEnter(e, "account")}
+                  onMouseEnter={(e) => handleMouseEnter(e, "contact")}
                   onMouseLeave={handleMouseLeave}
                 >
-                  My Account
-                </li>
-                <li
-                  ref={divRef}
-                  onMouseEnter={(e) => handleMouseEnter(e, "about")}
-                  onMouseLeave={handleMouseLeave}
-                >
-                  About
-                </li>
-                <li
-                  ref={divRef}
-                  onMouseEnter={(e) => handleMouseEnter(e, "insurance")}
-                  onMouseLeave={handleMouseLeave}
-                >
-                  Insurance
-                </li>
-                <li
-                  ref={divRef}
-                  onMouseEnter={(e) => handleMouseEnter(e, "news")}
-                  onMouseLeave={handleMouseLeave}
-                >
-                  News
+                  <div>
+                    <span className="button-dep">
+                      <a>Contact</a>
+                    </span>
+                  </div>
                 </li>
               </ul>
-            </nav>
-          </div>
-          <div className="contact-container">
-            <ul className="top">
-              <li
-                className="phone-number"
-                ref={divRef}
-                onMouseEnter={(e) => handleMouseEnter(e, "phone")}
-                onMouseLeave={handleMouseLeave}
-              >
-                <a className="a-standalone">(937) 374-0855</a>
-              </li>
-              <li
-                className="contact"
-                ref={divRef}
-                onMouseEnter={(e) => handleMouseEnter(e, "contact")}
-                onMouseLeave={handleMouseLeave}
-              >
-                <div>
-                  <span className="button-dep">
-                    <a>Contact</a>
-                  </span>
-                </div>
-              </li>
-            </ul>
+            </div>
           </div>
         </div>
-        <div></div>
-      </div>
-      <div className="top-roofline">
-        <div className="mover" style={{ width: cursorPosition.x }}></div>
-        <div className="cutout">
-          <img
-            src="./roofline.svg"
-            alt="arrow"
-            style={hoveredItem ? { opacity: "1" } : { opacity: "0" }}
+        <div className="top-roofline">
+          <div className="mover" style={{ width: cursorPosition.x }}></div>
+          <div className="cutout">
+            <img
+              src="./roofline.svg"
+              alt="arrow"
+              style={hoveredItem ? { opacity: "1" } : { opacity: "0" }}
+            />
+            <div
+              className="line"
+              style={!hoveredItem ? { opacity: "1" } : { opacity: "0" }}
+            ></div>
+          </div>
+          <div className="filler"></div>
+        </div>
+
+        <div className="mobileHeader">
+          <FaBars
+            size={24}
+            onClick={() => setIsOpen(!isOpen)}
+            className={`hamburger ${isOpen ? "open" : "close"}`}
           />
-          <div
-            className="line"
-            style={!hoveredItem ? { opacity: "1" } : { opacity: "0" }}
-          ></div>
+          <nav className={`nav-items ${isOpen ? "show" : "hide"}`}>
+            <a>Home</a>
+            <a>About</a>
+            <a>Contact</a>
+          </nav>
         </div>
-        <div className="filler"></div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
